105a45189542fa965932f34ab773ec22360214ba
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2023-03-26 11:04:11 -0600

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// import besan from "./assets/besan_barfi"
+export default function App(){
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    
+    <p className="text-6xl font-bold underline"  >Hello world</p>
 
-export default App;
+    <img src={require('./assets/besan_barfi.jpeg')} alt="besan" />
+    </>
+  )
+}
