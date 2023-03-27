f11f69b08e6fbe4f342d93d470f24717a940b432
satinder <satindersingh472@gmail.com>
2023-03-26 22:28:58 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import { BrowserRouter, Routes,Route} from "react-router-dom";
 
-// import besan from "./assets/besan_barfi"
-export default function App(){
-
-
+const App = () => {
   return (
     <>
-    
-    <p className="text-6xl font-bold underline"  >Hello world</p>
-
-    <img src={require('./assets/besan_barfi.jpeg')} alt="besan" />
+    <BrowserRouter>
+    <Routes>
+<Route path="/" element="<p>Hello world</p>"/>
+</Routes>
+</BrowserRouter>
     </>
   )
 }
+
+export default App;
---
+++ src/components/SweetsData.jsx
@@ -0,0 +1,8 @@
+const sweetsProducts = [
+    {
+        image: '../assets/alsi_pinni.jpeg',
+        productName: 'Desi Ghee Pinni',
+        productPrice: "$12.99/lb",
+        
+    }
+]
