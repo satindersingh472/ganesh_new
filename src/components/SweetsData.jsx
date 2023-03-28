99fdd505f5ff34b7ca5d789c3c245a4abf5a7eb8
satinder <satindersingh472@gmail.com>
2023-03-27 18:55:09 -0600

    working

--- src/components/SweetsData.jsx
+++ src/components/SweetsData.jsx
@@ -1,16 +1,19 @@
 const sweetsProducts = [
     {
+        image: "../assets/sweets/special_burfi.jpeg"
+    }
+    {
         image: '../assets/sweets/alsi_pinni.jpeg',
         productGroup: true,
-        productName: 'Desi Ghee Pinni',
+        mainProduct: 'Desi Ghee Pinni',
         productList: [{name:"Alsi",price:"$12.99/lb"},{name:"Atta",price:"$12.99/lb"},{name:"Besan",price:"$12.99/lb"}]
         
     },
     {
         image:'../assets/sweets/angoori_petha.jpeg',
         productGroup: false,
-        productName: "Angoori Petha",
-        price: "$9.49/lb"
+        mainProduct: "Angoori Petha",
+        productList: [{name:"Angoori Petha",price:"$12.99/lb"}]
     }
 ]
 
--- src/views/Sweets.jsx
+++ src/views/Sweets.jsx
@@ -2,24 +2,6 @@ import {alsi_pinni,angoori_petha} from "../assets/sweets";
 
 
 const Sweets = () => {
-  const sweetsProducts = [
-    {
-      image: `../assets/sweets/${alsi_pinni.jpeg}`,
-      productGroup: true,
-      productName: 'Desi Ghee Pinni',
-      productList: [
-        { name: 'Alsi', price: '$12.99/lb' },
-        { name: 'Atta', price: '$12.99/lb' },
-        { name: 'Besan', price: '$12.99/lb' },
-      ],
-    },
-    {
-      image: `../assets/sweets/${angoori_petha.jpeg}`,
-      productGroup: false,
-      productName: 'Angoori Petha',
-      price: '$9.49/lb',
-    },
-  ]
 
   return (
     <>
